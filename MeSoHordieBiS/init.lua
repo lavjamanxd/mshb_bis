4c3575513706dfa1c565458be62fcf328aed60da
lavjamanxd <lavjamanxd@gmail.com>
2022-05-20 22:17:44 +0200

    fix a soo dumb mistake :(

--- MeSoHordieBiS/init.lua
+++ MeSoHordieBiS/init.lua
@@ -1,3 +1,3 @@
-MeSoHordieAddon = LibStub("AceAddon-3.0"):NewAddon("MyAddon", "AceConsole-3.0", "AceHook-3.0", "AceEvent-3.0")
+MeSoHordieAddon = LibStub("AceAddon-3.0"):NewAddon("MeSoHordieAddon", "AceConsole-3.0", "AceHook-3.0", "AceEvent-3.0")
 MeSoHordieAddon.addonVersion = GetAddOnMetadata("MeSoHordieBiS", "Version")
 MeSoHordieAddon.lazyHooks = {}
