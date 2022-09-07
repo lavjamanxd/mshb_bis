f2950ef8c4e8ca749c6db871942d547902cf5d55
lavjamanxd <lavjamanxd@gmail.com>
2022-09-07 15:57:49 +0200

    remove end line characters form zip package

--- pack.sh
+++ pack.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 mkdir -p build
-zip -r build/MeSoHordieBiS_v`grep -E '## Version: (\d+\.\d+.\d+)' MeSoHordieBiS/MeSoHordieBiS.toc | cut -c 13-`.zip MeSoHordieBiS -x "*.DS_Store" -x "__MACOSX"
+zip -r build/MeSoHordieBiS_v`grep -E '## Version: (\d+\.\d+.\d+)' MeSoHordieBiS/MeSoHordieBiS.toc | cut -c 13- | rev | cut -c 2- | rev`.zip MeSoHordieBiS -x "*.DS_Store" -x "__MACOSX"
